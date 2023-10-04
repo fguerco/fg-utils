dd79101a469b960db114028d0dc49244c6ba3e7c
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-10-04 17:59:22 -0400

    defalias now creates aliases for macros too

--- src/aliases.lisp
+++ src/aliases.lisp
@@ -1,7 +1,10 @@
 (in-package :fg-utils)
 
-(defmacro defalias (alias function)
-  `(setf (symbol-function ',alias) #',function))
+(defmacro defalias (alias source)
+  (symbol-macrolet ((definition (if (macro-function source)
+                                    'macro-function
+                                    'symbol-function)))
+    `(setf (,definition ',alias) (,definition ',source))))
 
 (defalias filter remove-if-not)
 (defalias select remove-if-not)
