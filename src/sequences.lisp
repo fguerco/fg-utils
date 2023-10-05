994594e8d71c68d0bf70f76c5ae7ce19a0733693
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-10-04 23:29:01 -0400

    utilities of sequences of numbers

--- src/sequences.lisp
+++ src/sequences.lisp
@@ -1,5 +1,13 @@
 (in-package :fg-utils)
 
+(defmacro %loopseq-gen (var start end step op &rest forms)
+  (with-gensyms (startvar endvar stepvar)
+    `(let ((,startvar ,start) (,endvar ,end) (,stepvar ,step))
+       (loop
+         for ,var = ,startvar then (+ ,var ,stepvar)
+         until (= ,var (+ ,endvar ,stepvar))
+         ,op ,@forms))))
+
 (defmacro doseq ((var start end &optional (step 1)) &body forms)
   "Execute FORMS inside a sequence from START to END by STEP
 Examples:
@@ -7,24 +15,19 @@ Examples:
   (doseq (x 10 1 -1) (princ x)) => 10987654321
   (doseq (x 1 10 4) (princ x)) => 159
   (doseq (x 10 1 -4) (princ x)) => 1062"
-  (with-gensyms (startvar endvar stepvar)
-    `(loop
-       with ,startvar = ,start and ,endvar = ,end and ,stepvar = ,step
-       for ,var = ,startvar then (+ ,var ,stepvar)
-       until (funcall (if (plusp ,stepvar) #'> #'<) ,var ,endvar)
-       do ,@forms)))
+  `(%loopseq-gen ,var ,start ,end ,step do ,@forms))
 
 (defun seq (start end &optional (step 1))
   "Returns a sequence with a range from start to end. Use a negative step
 to generate a sequence backwards"
-  (let (result)
-    (doseq (x start end step) (push x result))
-    (nreverse result)))
+  (%loopseq-gen x start end step collect x))
 
 (defun mapseq (fn start end &optional (step 1))
   "Returns a list of applying FN to successive elements in the
 sequence defined"
-  (let (list)
-    (doseq (x start end step)
-      (push (funcall fn x) list))
-    (nreverse list)))
+  (%loopseq-gen x start end step collect (funcall fn x)))
+
+(defun mapseqn (fn start end &optional (step 1))
+  "Returns a concatenated list of applying FN to successive elements in the
+sequence defined, like mapcan"
+  (%loopseq-gen x start end step nconc (funcall fn x)))
