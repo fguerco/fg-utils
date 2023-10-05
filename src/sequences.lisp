19b48741ddd4c1f8dc0e65ed33ec88a2c0a71db5
Felipe Guerço Oliveira <felipeguerco@gmail.com>
2023-10-05 00:24:56 -0400

    making the loop generator more robust

--- src/sequences.lisp
+++ src/sequences.lisp
@@ -1,20 +1,26 @@
 (in-package :fg-utils)
 
 (defmacro %loopseq-gen (var start end step op &rest forms)
-  (with-gensyms (startvar endvar stepvar)
-    `(let ((,startvar ,start) (,endvar ,end) (,stepvar ,step))
+  (with-gensyms (startvar endvar stepvar compfn)
+    `(let ((,startvar ,start)
+           (,endvar ,end)
+           (,stepvar (abs ,step))
+           (,compfn #'<=))
+       (when (> ,startvar ,endvar)
+         (setf ,stepvar (- ,stepvar)
+               ,compfn #'>=))
        (loop
          for ,var = ,startvar then (+ ,var ,stepvar)
-         until (= ,var (+ ,endvar ,stepvar))
+         while (funcall ,compfn ,var ,endvar)
          ,op ,@forms))))
 
 (defmacro doseq ((var start end &optional (step 1)) &body forms)
   "Execute FORMS inside a sequence from START to END by STEP
 Examples:
   (doseq (x 1 10) (princ x)) => 12345678910
-  (doseq (x 10 1 -1) (princ x)) => 10987654321
+  (doseq (x 10 1 1) (princ x)) => 10987654321
   (doseq (x 1 10 4) (princ x)) => 159
-  (doseq (x 10 1 -4) (princ x)) => 1062"
+  (doseq (x 10 1 4) (princ x)) => 1062"
   `(%loopseq-gen ,var ,start ,end ,step do ,@forms))
 
 (defun seq (start end &optional (step 1))
